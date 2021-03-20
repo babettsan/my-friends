2327424d742a89333f837e5e5d848dc78ca83734
Yilmer Avila <yilmer0001@hotmail.com>
2021-03-20 10:50:32 -0500

    modify user form name email input and button class

--- src/components/addusers/AddUsers.js
+++ src/components/addusers/AddUsers.js
@@ -13,11 +13,11 @@ const Addusers = () => {
                 <label htmlFor="phone">phone</label>
             </div>
             <div className="form-floating mb-3">
-                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-                <label htmlFor="floatingInput">Email address</label>
+                <input name='email' type="email" className="form-control" id="email" placeholder="name@example.com"/>
+                <label htmlFor="email">Email address</label>
             </div>
             <div className="col-auto">
-                <button type="submit" className="btn btn-primary mb-3">Confirm identity</button>
+                <button type="submit" className="btn btn-success mb-3">Save friend</button>
             </div>
         </form>
     )
