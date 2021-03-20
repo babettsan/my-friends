46c10f101aac9bc1a61aa863566517be14591d05
Yilmer Avila <yilmer0001@hotmail.com>
2021-03-20 11:04:39 -0500

    control create user form using states

--- src/components/addusers/AddUsers.js
+++ src/components/addusers/AddUsers.js
@@ -8,21 +8,30 @@ const Addusers = () => {
     });
 
     function hadleSumbit(e){
-        e.preventDefault()
+        e.preventDefault();
+    }
+
+    function handleOnChange(event) {
+
+        setInput({
+            ...input,
+            //name : 'pedro'
+            [event.target.name] : event.target.value
+        })
     }
 
     return(
         <form className="form-floating mt-5 ms-5 me-5" onSubmit={hadleSumbit}>
             <div className="form-floating mb-3">
-                <input name='name' type="text" className="form-control" id="name" placeholder="Friend name"/>
+                <input value={input.name} name='name' type="text" className="form-control" id="name" placeholder="Friend name" onChange={handleOnChange}/>
                 <label htmlFor="name">Friend name</label>
             </div>
             <div className="form-floating mb-3">
-                <input name='phone' type="text" className="form-control" id="phone" placeholder="Friend phone" />
+                <input value={input.phone} name='phone' type="text" className="form-control" id="phone" placeholder="Friend phone" onChange={handleOnChange} />
                 <label htmlFor="phone">phone</label>
             </div>
             <div className="form-floating mb-3">
-                <input name='email' type="email" className="form-control" id="email" placeholder="name@example.com"/>
+                <input value={input.email} name='email' type="email" className="form-control" id="email" placeholder="name@example.com" onChange={handleOnChange}/>
                 <label htmlFor="email">Email address</label>
             </div>
             <div className="col-auto">
