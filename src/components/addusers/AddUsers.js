d07332e6e11fa7fc8b800f5aa7eee4bd20510b6b
Yilmer Avila <yilmer0001@hotmail.com>
2021-03-20 10:48:30 -0500

    modify user form name

--- src/components/addusers/AddUsers.js
+++ src/components/addusers/AddUsers.js
@@ -5,16 +5,19 @@ const Addusers = () => {
     return(
         <form className="form-floating mt-5 ms-5 me-5">
             <div className="form-floating mb-3">
-                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-                <label htmlFor="floatingInput">Email address</label>
+                <input name='name' type="text" className="form-control" id="name" placeholder="Friend name"/>
+                <label htmlFor="name">Friend name</label>
+            </div>
+            <div className="form-floating mb-3">
+                <input name='phone' type="text" className="form-control" id="phone" placeholder="Friend phone" />
+                <label htmlFor="phone">phone</label>
             </div>
             <div className="form-floating mb-3">
                 <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
                 <label htmlFor="floatingInput">Email address</label>
             </div>
-            <div className="form-floating mb-3">
-                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-                <label htmlFor="floatingInput">Email address</label>
+            <div className="col-auto">
+                <button type="submit" className="btn btn-primary mb-3">Confirm identity</button>
             </div>
         </form>
     )
